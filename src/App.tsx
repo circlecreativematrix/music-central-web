4d7cf1b03294676b5890e9cf32c11adfdc5e6c78
Robert Fornof <robertfornof@gmail.com>
2024-10-12 18:49:09 -0600

    working - notes play sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { JZZ } from 'jzz';
-import { SMF } from 'jzz-midi-smf';
-SMF(JZZ);
+import { useEffect, useState } from 'react'
+import JZZ from 'jzz'
+import SMF from 'jzz-midi-smf'
 import './App.css'
+SMF(JZZ)
+function midiPlay(smfIn: any){
+  const midiout = JZZ().openMidiOut([0,1,2,3,4]);
+  const player = smfIn.player();
+  console.log("dump", smfIn.dump())
+  player.connect(midiout);
+  player.play()
+}
+function midiToBase64Save(data: any){
+  //https://jazz-soft.net/demo/WriteMidiFile.html
+   // MIDI file dumped as a string
+  var b64 = JZZ.lib.toBase64(data); // convert to base-64 string
+  var uri = 'data:audio/midi;base64,' + b64; // data URI
+  //const a = document.createElement('a');
+  //a.href = uri;
+  //a.download = name||'itWorks.mid';
+  //a.click();
+  return uri
+}
 function midiOut(){
-  var smf = new SMF(0, 96); // type 0, 96 ticks per quarter note
+  var smf =JZZ.MIDI.SMF(2,96); // type 0, 96 ticks per quarter note
 var trk = new JZZ.MIDI.SMF.MTrk();
+
 smf.push(trk);
 // add contents:
 trk.add(0, JZZ.MIDI.smfSeqName('This is a sequence name'))
@@ -18,44 +35,42 @@ trk.add(0, JZZ.MIDI.smfSeqName('This is a sequence name'))
    .add(192, JZZ.MIDI.noteOff(0, 'C6'))
    .add(192, JZZ.MIDI.noteOff(0, 'Eb6'))
    .add(192, JZZ.MIDI.noteOff(0, 'G6'))
-   .add(288, JZZ.MIDI.smfEndOfTrack());
-// or an alternative way:
-trk.smfSeqName('This is a sequence name').smfBPM(90).tick(96)
-   .noteOn(0, 'C6', 127).noteOn(0, 'Eb6', 127).noteOn(0, 'G6', 127)
-   .tick(96).noteOff(0, 'C6').noteOff(0, 'Eb6').noteOff(0, 'G6')
-   .tick(96).smfEndOfTrack();
-// or even shorter:
-trk.smfSeqName('This is a sequence name').smfBPM(90).tick(96)
-   .ch(0).note('C6', 127, 96).note('Eb6', 127, 96).note('G6', 127, 96)
-   .tick(192).smfEndOfTrack();
+   .add(988, JZZ.MIDI.smfEndOfTrack());
+  
+   for (var i = 0; i < smf.length; i++) {
+    for (var j = 0; j < smf[i].length; j++) {
+      console.log('track:', i, 'tick:', smf[i][j].tt, smf[i][j].toString());
+      // or do whatever else with the message
+    }
+  }
+  return smf
 }
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [fileOut, SetFileOut] = useState("")
+ 
+  
+  useEffect(()=>{
+    const smf =  midiOut()
+    midiPlay(smf)
+    SetFileOut(midiToBase64Save(smf.dump()))
+    console.log("alldone!")
+  },[])
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href={fileOut} download="nameOfDownload.mid" target="_blank">
+          Click the logo to download the midi file
+          <img src="https://opensource.org/wp-content/uploads/2009/06/OSI_Standard_Logo_0.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  
     </>
   )
 }
 
 export default App
+function onEffect(arg0: () => void) {
+  throw new Error('Function not implemented.')
+}
+
